12e9d41b6a093af3f035d490129c04f0211d9ed9
ddtix <262985729+ddtix@users.noreply.github.com>
2026-03-04 23:32:35 +0300

    add router and autoload unpublished packages

--- public/index.php
+++ public/index.php
@@ -1,5 +1,20 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
+//require __DIR__ . '/../vendor/autoload.php';
+
+// подгрузка неопубликованных пакетов
+spl_autoload_register(function ($className) {
+    $parts = explode('\\', $className);
+    $fileName = array_pop($parts);
+    $folders = array_map('strtolower', $parts);
+    $filePath = __DIR__ . '/../vendor/' . implode('/', $folders) . '/src/' . $fileName . '.php';
+
+    if (file_exists($filePath)) {
+        require_once $filePath;
+    }
+});
 
 use Ddtix\Dbconn\DB;
 use Ddtix\Envloader\ENV;
+use Ddtix\Router\Route;
+
+ENV::load(__DIR__);
