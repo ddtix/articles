731e2d9ca0eb70ac0dbab0166cecc23b8a5526b9
ddtix <262985729+ddtix@users.noreply.github.com>
2026-03-05 19:08:37 +0300

    clean router

--- public/index.php
+++ public/index.php
@@ -1,20 +1,8 @@
 <?php
-// require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-// load unpublished packages
-spl_autoload_register(function ($className) {
-    $parts = explode('\\', $className);
-    $fileName = array_pop($parts);
-    $folders = array_map('strtolower', $parts);
-    $filePath = __DIR__ . '/../vendor/' . implode('/', $folders) . '/src/' . $fileName . '.php';
-
-    if (file_exists($filePath)) {
-        require_once $filePath;
-    }
-});
-
-use Ddtix\Dbconn\DB;
 use Ddtix\Envloader\ENV;
 use Ddtix\Router\Route;
 
 ENV::load(__DIR__);
+Route::load();
