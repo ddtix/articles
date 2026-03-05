654174b413b592e42576fe303c5ea183d25dfcb4
ddtix <262985729+ddtix@users.noreply.github.com>
2026-03-05 20:23:12 +0300

    add Index and NotFound Controllers

---
+++ src/Controllers/Index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Controllers;
+
+class Index
+{
+    public static function index(): string
+    {
+        return "Index";
+    }
+}
---
+++ src/Controllers/NotFound.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Controllers;
+
+class NotFound
+{
+    public static function index(): string
+    {
+        return "NotFound";
+    }
+}
